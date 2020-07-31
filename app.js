e18672e181a04f99110e14c3d6403dfed2cbc61f
fishwickid <chadfishwick@gmail.com>
2020-07-31 21:11:19 +0800

    add employee role

--- app.js
+++ app.js
@@ -27,14 +27,8 @@ function runSearch() {
         "Show all departments",
         "Add employee",
         "Add role",
-        "Department",
-        // "Update employee manager",
-        "View all roles",
-        "Add Role",
-        "Remove role",
-        "View all departments",
-        "Add department",
-        "Remove department",
+        "Add Department",
+        "Update employee role",
         "exit"
       ]
     })
@@ -61,35 +55,7 @@ function runSearch() {
         break;
 
       case "Add department":
-        addEmployee();
-        break;
-
-      case "Update employee manager":
-        updateEmployeeManager();
-        break;
-
-      case "View all roles":
-        viewAllRoles();
-        break;
-
-      case "Add Role":
-        addRole();
-        break;
-
-      case "Remove role":
-        removeRoll();
-        break;
-
-      case "View all departments":
-        viewAllDepartments();
-        break;
-
-      case "Add department":
         addDepartment();
-        break;
-
-      case "Remove department":
-        removeDepartment();
         break;
 
       case "exit":
@@ -202,32 +168,6 @@ function rangeSearch() {
               res[i].year
           );
         }
-        runSearch();
-      });
-    });
-}
-
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to look for?"
-    })
-    .then(function(answer) {
-      console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        if (err) throw err;
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
         runSearch();
       });
     });
